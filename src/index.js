634c6568dddf4566a020fcb69cb10178f3820e9b
Cristian Villa <c.villavirgen@gmail.com>
2023-08-01 18:37:13 -0600

    Add files, update index.html

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Leaderboard from './modules/leaderBoard.js';
+import listScores from './modules/listScores.js';
+
+const leaderboard = new Leaderboard();
+
+document.getElementById('submit').addEventListener('click', () => {
+  const newName = document.getElementById('new-name').value;
+  const newScore = document.getElementById('new-score').value;
+  leaderboard.addItem(newName, newScore);
+  listScores(leaderboard.list);
+});
